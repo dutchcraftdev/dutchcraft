22a493ef5507852bc9ffa9681d51e0bf3c6e2288
Dutchman <dutchcraft_contact@proton.me>
2023-04-15 19:20:54 -0400

    Initial commit

---
+++ src/main/java/net/dutch/dutchcraft/item/LogoItem.java
@@ -0,0 +1,43 @@
+
+package net.dutch.dutchcraft.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.dutch.dutchcraft.DutchcraftModElements;
+
+@DutchcraftModElements.ModElement.Tag
+public class LogoItem extends DutchcraftModElements.ModElement {
+	@ObjectHolder("dutchcraft:logo")
+	public static final Item block = null;
+
+	public LogoItem(DutchcraftModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(null).maxStackSize(64).rarity(Rarity.EPIC));
+			setRegistryName("logo");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/net/dutch/dutchcraft/itemgroup/DUTCHCRAFTItemGroup.java
@@ -0,0 +1,36 @@
+
+package net.dutch.dutchcraft.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.dutch.dutchcraft.item.LogoItem;
+import net.dutch.dutchcraft.DutchcraftModElements;
+
+@DutchcraftModElements.ModElement.Tag
+public class DUTCHCRAFTItemGroup extends DutchcraftModElements.ModElement {
+	public DUTCHCRAFTItemGroup(DutchcraftModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabdutchcraft") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(LogoItem.block);
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+
+	public static ItemGroup tab;
+}
