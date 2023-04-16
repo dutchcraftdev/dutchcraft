cee4e5dcb4f136a87ab6801d2a0023e6cc2688fe
Dutchman <dutchcraft_contact@proton.me>
2023-04-15 20:06:14 -0400

    lucas o'reiley

---
+++ src/main/java/net/dutch/dutchcraft/item/FryingPanItem.java
@@ -0,0 +1,52 @@
+
+package net.dutch.dutchcraft.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.dutch.dutchcraft.itemgroup.DUTCHCRAFTItemGroup;
+import net.dutch.dutchcraft.DutchcraftModElements;
+
+@DutchcraftModElements.ModElement.Tag
+public class FryingPanItem extends DutchcraftModElements.ModElement {
+	@ObjectHolder("dutchcraft:frying_pan")
+	public static final Item block = null;
+
+	public FryingPanItem(DutchcraftModElements instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 50;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 23f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3.7f, new Item.Properties().group(DUTCHCRAFTItemGroup.tab).isImmuneToFire()) {
+		}.setRegistryName("frying_pan"));
+	}
+}
